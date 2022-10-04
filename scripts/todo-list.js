99aadb6d1660d72669ca413baebcee2aa139cd24
dstingaciu <dsstingaciu@gmail.com>
2022-10-04 00:02:20 -0400

    Added ToDoListData with static methods

---
+++ scripts/todo-list.js
@@ -0,0 +1,32 @@
+class ToDoList {
+  static ID = 'todo-list';
+  
+  static FLAGS = {
+    TODOS: 'todos'
+  }
+  
+  static TEMPLATES = {
+    TODOLIST: `modules/${this.ID}/templates/todo-list.hbs`
+  }
+}
+
+class ToDoListData {
+  static getToDosForUser() {
+    return game.user().getFlag(ToDoList.ID, ToDoList.FLAGS.TODOS);
+  }
+
+  static createToDo(toDoData) {
+    const newToDo = {
+      isDone: false,
+      id: foundry.utils.randomID(16),
+      userId,
+      ...toDoData,
+    }
+
+    const newToDos = {
+      [newToDo.id]: newToDo
+    }
+
+    return game.user().setFlag(ToDoList.ID, ToDoList.FLAGS.TODOS, newToDos);
+  }
+}
